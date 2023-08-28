bf0efcb215c8d253f86205d83dec2b82b1bc7ed8
Muhammad Tayyab <mtbahaudin@taifco.net>
2023-08-28 19:59:51 +0500

    Check one

---
+++ test/config_test.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('App Config', () async {
+    WidgetsFlutterBinding.ensureInitialized();
+    final configString = await rootBundle.loadString('lib/config.json');
+    final config = json.decode(configString) as Map<String, dynamic>;
+    String _apiUrl = config['apiUrl'] as String;
+
+    expect(_apiUrl, "https://api-url.com");
+  });
+}
